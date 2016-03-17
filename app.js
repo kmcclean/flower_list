1fb6a2c065937b180a8bbde44b162fc3f91d10c2
clara <clara@clara.com>
2016-03-16 21:55:08 -0500

    homepage, filter by color

--- app.js
+++ app.js
@@ -17,11 +17,33 @@ MongoClient.connect('mongodb://localhost:27017/garden', function(err, db){
 
   //routes - this is for the home page
   app.get('/', function(req, res){
-    db.collection('flowers').find().toArray(function(err, flowerdocs){
+    // Make query for unique colors, to populate the dropdown box
+    db.collection('flowers').distinct('color', function(err, colorDocs) {
       if (err) { return res.sendStatus(500); }
-      return res.render('allflowers', { 'flowers' : flowerdocs });
-    });
+
+      //If selecting a color and clicked the colorButton, this has a value
+      if (req.query.colorButton) {
+        var color = req.query.colorDropDown;    //What color was selected?
+        //Get all of the flowers of the desired color.
+        db.collection('flowers').find({'color': color}).toArray(function(err, flowerDocs) {
+          if (err) { return res.sendStatus(500); }
+          //Optional - turn 'red' into 'Red' for display
+          var displayColor = color.slice(0,1).toUpperCase() + color.slice(1, color.length)
+          //return res.render statement inside a callback to prevent further processing of response
+          return res.render('allflowers',
+             { 'flowers' : flowerDocs, 'currentColor' : displayColor, 'flowerColors' : colorDocs } );
+        });
+      }
+
+    //If visiting the home page, or clicking the clear button, then fetch all of the flowers
+      else {
+        db.collection('flowers').find().toArray(function(err, flowerDocs){
+          if (err) { return res.sendStatus(500); }
+          return res.render('allFlowers', { 'flowers' : flowerDocs, 'flowerColors' : colorDocs } );
+        });
+      }
   });
+});
 
   //All other requests, return 404 not found
   app.use(function(req, res){
